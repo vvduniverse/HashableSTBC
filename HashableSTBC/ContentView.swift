aad3ad9658dd77390c165474e45350e176729b99
Vahtee Boo <vahtee.boo@gmail.com>
2022-03-25 12:48:28 +0200

    done

--- HashableSTBC/ContentView.swift
+++ HashableSTBC/ContentView.swift
@@ -7,10 +7,35 @@
 
 import SwiftUI
 
+struct MyCustomModel: Hashable {
+    let title: String
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(title)
+    }
+}
+
 struct ContentView: View {
+    let data = [
+    MyCustomModel(title: "ONE"),
+    MyCustomModel(title: "TWO"),
+    MyCustomModel(title: "THREE"),
+    MyCustomModel(title: "FOUR"),
+    MyCustomModel(title: "FIVE"),
+    MyCustomModel(title: "SIX"),
+    MyCustomModel(title: "SEVEN"),
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            VStack(spacing: 20) {
+                ForEach(data, id: \.self) { item in
+                    Text(item.title)
+                    Text(item.hashValue.description)
+                        .font(.headline)
+                }
+            }
+        }
     }
 }
 
