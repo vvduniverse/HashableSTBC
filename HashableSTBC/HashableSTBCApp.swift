bc975a7808ea826016588594624cda03a2047b7b
Vahtee Boo <vahtee.boo@gmail.com>
2022-03-25 12:32:56 +0200

    Initial Commit

---
+++ HashableSTBC/HashableSTBCApp.swift
@@ -0,0 +1,17 @@
+//
+//  HashableSTBCApp.swift
+//  HashableSTBC
+//
+//  Created by Vahtee Boo on 25.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct HashableSTBCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
